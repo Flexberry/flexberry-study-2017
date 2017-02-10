594d61fa4196ac7f38545ef3005573acb935177d
aepanov <aae111k@gmail.com>
2017-02-10 14:37:46 +0500

    Практическая работа №1. Вариант 06.

---
+++ Case06/ContractsAccount/AcademicPerformance/FindDogovor.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="Найти договор" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="FindDogovor.aspx.cs" Inherits="AcademicPerformance.FindDogovor" %>
+
+<%@ Register TagPrefix="DogovorControls" TagName="GetDogovor" Src="~/GetDogovor.ascx" %>
+<asp:Content ID="BodyContent" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+     <DogovorControls:GetDogovor ID="GetDogovor1" runat="server" />
+</asp:Content>
---
+++ Case06/ContractsAccount/AcademicPerformance/GetDogovor.ascx.designer.cs
@@ -0,0 +1,87 @@
+﻿//------------------------------------------------------------------------------
+// <автоматически создаваемое>
+//     Этот код создан программой.
+//
+//     Изменения в этом файле могут привести к неправильной работе и будут потеряны в случае
+//     повторной генерации кода. 
+// </автоматически создаваемое>
+//------------------------------------------------------------------------------
+
+namespace AcademicPerformance {
+    
+    
+    public partial class GetDogovor {
+        
+        /// <summary>
+        /// LabelCode элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelCode;
+        
+        /// <summary>
+        /// TextBoxCode элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxCode;
+        
+        /// <summary>
+        /// ButtonFind элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ButtonFind;
+        
+        /// <summary>
+        /// PanelDogovor элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel PanelDogovor;
+        
+        /// <summary>
+        /// LabelNumber элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelNumber;
+        
+        /// <summary>
+        /// LabelInformation элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelInformation;
+        
+        /// <summary>
+        /// PanelDogovorIsNotFound элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel PanelDogovorIsNotFound;
+        
+        /// <summary>
+        /// Label1 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+    }
+}
