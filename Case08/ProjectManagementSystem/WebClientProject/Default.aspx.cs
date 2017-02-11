fcd70f5ee5b9a47f29dec003c0dd1f58fcc3dbee
Golovin Vlad <vladick95@gmail.com>
2017-02-11 15:45:45 +0500

    Добавил клиентское приложение

---
+++ Case08/ProjectManagementSystem/WebClientProject/Default.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace WebClientProject
+{
+    using System.Web.UI;
+    using System.Web.UI.WebControls;
+    using WorkTimeLibrary;
+    using ManagementSystemObjects;
+
+    public partial class _Default : Page
+    {
+        private List<Day> days;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            days = new List<Day>();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DateTime startDate = DateTime.Parse(TextBoxStartDate.Text);
+                int hour = Int32.Parse(TextBoxHour.Text);
+
+                WorkTimeBuilder wtb = new WorkTimeBuilder();
+                DeadLineCalculator deadLineCalculator = new DeadLineCalculator();
+                DateTime deadLine = deadLineCalculator.CalculateDeadLine(hour, startDate, wtb);
+
+                finishDate.Text = deadLine.ToString();
+                ErrorLabel.Visible = false;
+            }
+            catch (System.FormatException)
+            {
+                ErrorLabel.Visible = true;
+            }
+        }
+    }
+}
