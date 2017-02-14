3177d52472ad8b2d1e86aeff5f0f9456cbb517a9
NSelivanov <selivanov.nick@gmail.com>
2017-02-14 12:05:18 +0500

    Init TASK12.Chapter 2

---
+++ Case12/Praktic_2/TableTennisTournament/TableTennisTournament.Objects/Match.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TableTennisTournament.Objects
+{
+    public class Match
+    {
+        
+        public Guid MatchGuid
+        { get; set; }
+        public int GameFactor { get; set; }
+        public DateTime PlayDateTime { get; set; }
+        public Player PlayerOne { get; set; }
+        public Player PlayerTwo { get; set; }
+        public int ScorePlayerOne { get; set; }
+        public int ScorePlayerTwo { get; set; }
+        public double Result { get; set; }
+        public bool IsRegistred { get; set; }
+    }
+}
