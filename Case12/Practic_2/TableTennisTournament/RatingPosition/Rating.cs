43f8ee25545b82684ed76d9f2c2eae4a3c6be43c
NSelivanov <selivanov.nick@gmail.com>
2017-02-14 11:47:58 +0500

    Init the TASK12. chapter 2.

---
+++ Case12/Practic_2/TableTennisTournament/RatingPosition/Rating.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TableTennisTournament.Objects;
+
+namespace RatingPosition
+{
+    public class Rating
+    {
+        private static int magicK = 16;
+
+        private static double FutureBonus(double ratingA, double ratingB)
+        {
+            var pow = (ratingB - ratingA)/400;
+            return 1/(1 + Math.Pow(10, pow));
+        }
+
+        public static int GetNewRating(int currentRating,double matchResult, double bonusRating)
+        {
+            return currentRating + (int) Math.Round(magicK * (matchResult - bonusRating));
+        }
+
+        public static void GetNewPlayerRating(Match match)
+        {
+            magicK = match.GameFactor;
+            var matchResult_ViewA = match.Result;
+            var matchResult_ViewB = 1 - matchResult_ViewA;
+            var bonus_ViewA = FutureBonus(match.PlayerOne.Rating,match.PlayerTwo.Rating);
+            var bonus_ViewB= 1-bonus_ViewA;
+            match.PlayerOne.Rating = GetNewRating(match.PlayerOne.Rating, matchResult_ViewA, bonus_ViewA);
+            match.PlayerTwo.Rating = GetNewRating(match.PlayerTwo.Rating, matchResult_ViewB, bonus_ViewB);
+        }
+    }
+}
---
+++ Case12/Practic_2/TableTennisTournament/TableTennisTournament.Tests/UnitTest1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TableTennisTournament.Objects;
+
+namespace TableTennisTournament.Tests
+{
+    using RatingPosition;
+    [TestClass]
+    public class UnitTest1
+    {
+        Player player_MAX = new Player()
+        {
+            Login = "Player_Max",
+            Rating = 2400
+        };
+        Player player_middle = new Player()
+        {
+            Login = "Player_middle",
+            Rating = 1200
+        };
+
+
+        //[TestMethod]
+        //public void Bonus_zero_max()
+        //{
+        //    var looser = Rating.FutureBonus(0,1200);
+        //    var power = Rating.FutureBonus(1200, 0);
+        //    var sum = Math.Round(looser + power, 3);
+        //    Assert.AreEqual(1,sum);
+        //}
+
+        //[TestMethod]
+        //public void Bonus_zero_about_equal()
+        //{
+        //    var looser = Rating.FutureBonus(1000, 1200);
+        //    var power = Rating.FutureBonus(1200, 1000);
+        //    var sum = Math.Round(looser + power,3);
+        //    Assert.AreEqual(1, sum);
+        //}
+
+        //[TestMethod]
+        //public void Bonus_equal_win()
+        //{
+        //    var power = Rating.FutureBonus(1200, 1200);
+        //    Assert.AreEqual(0.5, power);
+        //}
+
+        [TestMethod]
+        public void Rating_Looser_Win()
+        {
+            player_middle.Rating = 1200;
+            player_MAX.Rating = 2400;
+            var match = new Match()
+            {
+                GameFactor = 16,
+                PlayerOne = player_middle,
+                PlayerTwo = player_MAX,
+                Result = 1
+            };
+           Rating.GetNewPlayerRating(match);
+            
+            Assert.AreEqual(1216,match.PlayerOne.Rating);
+            Assert.AreEqual(2384, match.PlayerTwo.Rating);
+        }
+
+        [TestMethod]
+        public void Rating_Powerfull_Win()
+        {
+            player_middle.Rating = 1200;
+            player_MAX.Rating = 1300;
+            var match = new Match()
+            {
+                GameFactor = 16,
+                PlayerOne = player_middle,
+                PlayerTwo = player_MAX,
+                Result = 0
+            };
+            Rating.GetNewPlayerRating(match);
+           
+            Assert.AreEqual(1194, match.PlayerOne.Rating);
+            Assert.AreEqual(1306, match.PlayerTwo.Rating);
+        }
+
+        [TestMethod]
+        public void Rating_Pair()
+        {
+            player_middle.Rating = 1200;
+            player_MAX.Rating = 1300;
+            var match = new Match()
+            {
+                GameFactor = 16,
+                PlayerOne = player_middle,
+                PlayerTwo = player_MAX,
+                Result = 0.5
+            };
+            Rating.GetNewPlayerRating(match);
+
+            Assert.AreEqual(1202, match.PlayerOne.Rating);
+            Assert.AreEqual(1298, match.PlayerTwo.Rating);
+        }
+    }
+}
