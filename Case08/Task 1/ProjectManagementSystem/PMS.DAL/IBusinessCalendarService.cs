3755d6c922c01cca182b5bb68b772ddc1cdb0326
Golovin Vlad <vladick95@gmail.com>
2017-02-14 14:21:03 +0500

    Полностью выполнил задание 1

---
+++ Case08/Task 1/ProjectManagementSystem/PMS.DAL/IBusinessCalendarService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMS.DAL
+{
+    using PMS.Objects;
+
+    /// <summary>
+    /// Интерфейс доступа к информации о днях
+    /// </summary>
+    public interface IBusinessCalendarService
+    {
+        IEnumerable<Day> GetDaysCollection(DateTime dateStart, DateTime dateFinish);
+    }
+}
