6797f5e1fa4aa39c07793207be56c11f80823c96
PMasalkin <pashamasalkin@yandex.ru>
2017-03-03 13:16:24 +0500

    Добавли не рабочее задание 2.

---
+++ Case13/Задание 2/Lab2/Lab2/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab2.Startup))]
+namespace Lab2
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
--- Case13/Задание 3/SQLLab3sql.sql
+++ Case13/Задание 3/SQLLab3sql.sql
@@ -2,7 +2,7 @@
 -- Table userBase
 -- -----------------------------------------------------
 CREATE TABLE userBase (
-  id INT NOT NULL,
+  id int IDENTITY(1,1) PRIMARY KEY,
   login VARCHAR(45) NULL,
   mail VARCHAR(45) NULL,
   surname VARCHAR(45) NULL,
@@ -12,15 +12,16 @@ CREATE TABLE userBase (
   birthday DATE NULL,
   sity VARCHAR(45) NULL,
   country VARCHAR(45) NULL,
-  website VARCHAR(45) NULL,
-  PRIMARY KEY (id));
+  website VARCHAR(45) NULL);
+
+  alter table userBase  add constraint con_userBase_mail unique (mail)
 
 
 -- -----------------------------------------------------
 -- Table profile
 -- -----------------------------------------------------
 CREATE TABLE profile (
-  id INT NOT NULL,
+  id int IDENTITY(1,1),
   login VARCHAR(45) NULL,
   mail VARCHAR(45) NULL,
   surname VARCHAR(45) NULL,
@@ -42,57 +43,73 @@ CREATE TABLE profile (
 CREATE TABLE contact (
   UserId INT NOT NULL,
   ContactId VARCHAR(45) NOT NULL,
-  PRIMARY KEY (UserId, ContactId ));
+  PRIMARY KEY (UserId, ContactId));
+
+  alter table contact  add constraint con_contact unique (UserId, ContactId)
 
   -- -----------------------------------------------------
 -- Table mydb.network
 -- -----------------------------------------------------
 CREATE TABLE network (
-  idnetwork INT NOT NULL,
+  id int IDENTITY(1,1) PRIMARY KEY,
   name VARCHAR(45) NULL,
-  website VARCHAR(45) NULL,
-  PRIMARY KEY (idnetwork));
+  website VARCHAR(45) NULL);
+
+  alter table network  add constraint con_network_web unique (name, website)
 
 
-INSERT INTO userBase (id, login, mail, surname, name, patronymic, gender, birthday, sity, country, website)
+INSERT INTO userBase (login, mail, surname, name, patronymic, gender, birthday, sity, country, website)
 VALUES 
-('1', 'login1', 'mail1', 'surname1', 'name1', 'patronymic1', 'm', '12.12.2000', 'sity1', 'country1', 'website1'),
-('2', 'login2', 'mail2', 'surname2', 'name2', 'patronymic2', 'f', '2.02.2002', 'sity2', 'country2', 'website2'),
-('3', 'login3', 'mail3', 'surname3', 'name3', 'patronymic3', 'm', '3.02.2003', 'sity3', 'country3', 'website3'),
-('4', 'login4', 'mail4', 'surname4', 'name4', 'patronymic4', 'f', '4.02.2004', 'sity4', 'country4', 'website4'),
-('5', 'login5', 'mail5', 'surname5', 'name5', 'patronymic5', 'm', '5.02.2005', 'sity5', 'country5', 'website5');
+( 'login1', 'mail1', 'surname1', 'name1', 'patronymic1', 'm', '12.12.2000', 'sity1', 'country1', 'website1'),
+( 'login2', 'mail2', 'surname2', 'name2', 'patronymic2', 'f', '2.02.2002', 'sity2', 'country2', 'website2'),
+( 'login3', 'mail3', 'surname3', 'name3', 'patronymic3', 'm', '3.02.2003', 'sity3', 'country3', 'website3'),
+( 'login4', 'mail4', 'surname4', 'name4', 'patronymic4', 'f', '4.02.2004', 'sity4', 'country4', 'website4'),
+( 'login5', 'mail5', 'surname5', 'name5', 'patronymic5', 'm', '5.02.2005', 'sity5', 'country5', 'website5');
 
-INSERT INTO profile (id, login, mail, surname, name, patronymic, gender, birthday, sity, country, website, User_id, idnetwork)
+INSERT INTO profile ( login, mail, surname, name, patronymic, gender, birthday, sity, country, website, User_id, idnetwork)
 VALUES 
-('1', 'login1', 'mail1', 'surname1', 'name1', 'patronymic1', 'm', '12.12.2000', 'sity1', 'country1', 'website1', 1, 1),
-('2', 'login2', 'mail2', 'surname2', 'name2', 'patronymic2', 'f', '2.02.2002', 'sity2', 'country2', 'website2', 2, 1),
-('3', 'login3', '', 'surname3', 'name3', 'patronymic3', 'm', '3.02.2003', 'sity3', 'country3', 'website3', 3, 1),
-('4', 'login4', 'mail4', 'surname4', '', 'patronymic4', 'f', '4.02.2004', 'sity4', 'country4', 'website4', 4, 1),
-('5', 'login5', 'mail5', 'surname5', 'name5', 'patronymic5', 'm', '5.02.2005', 'sity5', 'country5', 'website5', 5, 1),
-('6', 'login1', 'mail1', 'surname1', 'name1', 'patronymic1', 'm', '12.12.2000', 'sity1', 'country1', 'website1', 1, 2),
-('7', 'login2', 'mail2', 'surname2', 'name2', '', 'f', '2.02.2002', 'sity2', 'country2', 'website2', 2, 3);
+( 'login1', 'mail1', 'surname1', 'name1', 'patronymic1', 'm', '12.12.2000', 'sity1', 'country1', 'website1', 1, 1),
+( 'login2', 'mail2', 'surname2', 'name2', 'patronymic2', 'f', '2.02.2002', 'sity2', 'country2', 'website2', 2, 1),
+( 'login3', '', 'surname3', 'name3', 'patronymic3', 'm', '3.02.2003', 'sity3', 'country3', 'website3', 3, 1),
+( 'login4', 'mail4', 'surname4', '', 'patronymic4', 'f', '4.02.2004', 'sity4', 'country4', 'website4', 4, 1),
+( 'login5', 'mail5', 'surname5', 'name5', 'patronymic5', 'm', '5.02.2005', 'sity5', 'country5', 'website5', 5, 1),
+( 'login1', 'mail1', 'surname1', 'name1', 'patronymic1', 'm', '12.12.2000', 'sity1', 'country1', 'website1', 1, 2),
+( 'login2', 'mail2', 'surname2', 'name2', '', 'f', '2.02.2002', 'sity2', 'country2', 'website2', 2, 3),
+( 'login4', 'mail2', 'surname2', 'name2', '', 'f', '2.05.2002', 'sity2', 'country2', 'website2', 2, 3);
 
-INSERT INTO profile (UserId, ContactId)
+INSERT INTO contact(UserId, ContactId)
 VALUES 
 ('1', '2'),
 ('1', '3'),
 ('2', '1'),
 ('3', '2');
 
+INSERT INTO network( name, website)
+VALUES 
+( 'vk', 'vk.com'),
+( 'facebook', 'facebook.com'),
+( 'google', 'google.com');
 
-1) SELECT userID, COUNT(userID) From dbo.contact GROUP BY userID
 
-2) SELECT TOP 1 surname, COUNT(surname) From dbo.userBase GROUP BY surname 
+/*
+1)  SELECT userID, COUNT(userID) From dbo.contact GROUP BY userID
 
-3)
+2)  SELECT TOP 1 surname, COUNT(surname) From dbo.userBase GROUP BY surname 
 
-4) SELECT COUNT(userID)/COUNT(id) From dbo.contact, dbo.userBase
+3)  Select  dbo.network.name, DATEPART(YEAR, birthday), gender, COUNT(*) as profile_count From dbo.profile, dbo.network 
+	Where dbo.profile.idnetwork=dbo.network.id 
+	group by dbo.network.name, DATEPART(YEAR, birthday), gender
+	ORDER BY dbo.network.name, DATEPART(YEAR, birthday), gender
 
-5)Declare 
-	@male_count INT,
-	@male_percent Real,
-	@female_percent Real
-SELECT @male_count=COUNT(gender) From dbo.userBase  Where gender='m' GROUP BY gender
-SELECT @male_percent=@male_count*100/COUNT(id), @female_percent=(COUNT(id)-@male_count)*100/COUNT(id) From dbo.userBase
-PRINT N'M:'+CAST(@male_percent AS VARCHAR(2))+N'%'
-PRINT N'F:'+CAST(@female_percent AS VARCHAR(2))+N'%'
+
+4)  SELECT COUNT(userID)/COUNT(id) From dbo.contact, dbo.userBase
+
+5)	Declare 
+		@male_count INT,
+		@male_percent Real,
+		@female_percent Real
+	SELECT @male_count=COUNT(gender) From dbo.userBase  Where gender='m' GROUP BY gender
+	SELECT @male_percent=@male_count*100/COUNT(id), @female_percent=(COUNT(id)-@male_count)*100/COUNT(id) From dbo.userBase
+	PRINT N'M:'+CAST(@male_percent AS VARCHAR(2))+N'%'
+	PRINT N'F:'+CAST(@female_percent AS VARCHAR(2))+N'%'
+	*/
