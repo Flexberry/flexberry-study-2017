00bf15ecd2c866339262d283d7b65db14b00d2c1
Evgeniy Khaberev <ehaberev@yandex.ru>
2017-03-16 13:50:14 +0500

    Add example for task 6

---
+++ Example/Task 6/RecordBookBL/CodeGenerator/CodeGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace NewPlatform.RecordBookBL
+{
+    using System.IO;
+    using System.IO.Compression;
+
+    public static class CodeGenerator
+    {
+        public static string GetCodeForRecordBook(Студент студент)
+        {
+            var кодСпециальности = студент.Группа.Специальность.КодСпециальности;
+            if (студент.Фамилия == null || студент.Имя == null || студент.Отчество == null)
+            {
+                throw new ArgumentNullException(nameof(студент), "У студента не указано полное ФИО");
+            }
+
+            var знак1 = студент.Фамилия.Substring(0, 1);
+            var знак2 = студент.Имя.Substring(0, 1);
+            var знак3 = студент.Отчество.Substring(0, 1);
+            var суффикс = $"{студент.ДатаРождения.Month}{студент.ДатаРождения.Day}";
+            return $"{кодСпециальности}-{знак1}{знак2}{знак3}{суффикс}";
+        }
+    }
+}
