ffaa005b1f3ac9f51a47d435f7387fe09be112c8
alexandrMperm <alex.serg.minin@gmail.ru>
2017-03-22 00:37:02 +0500

    Edit stage add code
    add code

---
+++ Case05/Task4/CodeGen/TeploUchetCode/TeploUchet/ASP.NET/Default.aspx.cs
@@ -0,0 +1,11 @@
+﻿namespace ICSSoft.STORMNET.Web
+{
+    using ICSSoft.STORMNET.Web.AjaxControls.Forms;
+
+    /// <summary>
+    /// Класс страницы по-умолчанию.
+    /// </summary>
+    public partial class Default : BasePage
+    {
+    }
+}
---
+++ Case05/Task4/CodeGen/TeploUchetCode/TeploUchet/ASP.NET/FormUtils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ICSSoft.STORMNET.Web
+{
+    using ICSSoft.STORMNET.Web.AjaxControls;
+    using ICSSoft.STORMNET;
+    using Resources;
+
+    /// <summary>
+    /// Статические утилиты для работы с формами
+    /// </summary>
+    public static class FormUtils
+    {
+        /// <summary>Смена настроек лукапов</summary>
+        /// <param name="lookup">Лукап, которому меняются настройки</param>
+        public static void ChangeLookUpSettings(BaseMasterEditorLookUp lookup)
+        {
+            try
+            {
+                Type type = Type.GetType(lookup.MasterTypeName);
+                string caption = Information.GetClassCaption(type);
+                lookup.LookUpFormCaption = !string.IsNullOrEmpty(caption) ? caption : Resource.Select_Value;
+            }
+            catch (Exception)
+            {
+                lookup.LookUpFormCaption = Resource.Select_Value;
+            }
+        }
+    }
+}
