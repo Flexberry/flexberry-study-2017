33cf7a07ec271b9701e14b804c4be5955c05c247
Golovin Vladislav <vgolovin@ics.perm.ru>
2017-03-28 16:59:29 +0500

    Remove

--- Case08/Task 5/BusinessCalendar/ASP.NET/shared/script/exception-day-edit.js
+++
@@ -1,8 +0,0 @@
-﻿$(document).ready(function () {
-    $('.businessTimeSpans').TimeSpans("init", $('#wtsJson').val());
-    
-})
-function saveJsonArray() {
-    var tsArray = JSON.stringify($('.businessTimeSpans').TimeSpans("getTimeSpans"));
-    document.getElementById("wtsJson").value = tsArray;
-}
