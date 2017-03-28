81dda6e967b24ccd083928a1d2330b694cfd0fa0
Golovin Vladislav <vgolovin@ics.perm.ru>
2017-03-28 18:02:02 +0500

    Added solution for Task 5

---
+++ Case08/Task 5/BusinessCalendar/Objects/braces(28.03.2017 17-49)WorkTimeDefinition.cs
@@ -0,0 +1,12 @@
+*** Start programmer edit section *** (WorkTimeDefinition.SumTime CustomAttributes)
+
+*** End programmer edit section *** (WorkTimeDefinition.SumTime CustomAttributes)
+
+*** Start programmer edit section *** (WorkTimeDefinition.SumTime Get)
+                return null;
+*** End programmer edit section *** (WorkTimeDefinition.SumTime Get)
+
+*** Start programmer edit section *** (WorkTimeDefinition.SumTime Set)
+
+*** End programmer edit section *** (WorkTimeDefinition.SumTime Set)
+
