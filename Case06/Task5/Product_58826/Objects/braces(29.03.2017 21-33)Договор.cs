1d51a57c39c60e6ca7a0caf4ed45dd68fc39ff4e
aepanov <aae111k@gmail.com>
2017-03-29 22:48:46 +0500

    Добавил задание 5. Путаница из-за внешних ключей.

---
+++ Case06/Task5/Product_58826/Objects/braces(29.03.2017 21-33)Договор.cs
@@ -0,0 +1,12 @@
+*** Start programmer edit section *** (Договор.Интервал CustomAttributes)
+
+*** End programmer edit section *** (Договор.Интервал CustomAttributes)
+
+*** Start programmer edit section *** (Договор.Интервал Get)
+                return null;
+*** End programmer edit section *** (Договор.Интервал Get)
+
+*** Start programmer edit section *** (Договор.Интервал Set)
+
+*** End programmer edit section *** (Договор.Интервал Set)
+
---
+++ Case06/Task5/Product_58826/Objects/braces(29.03.2017 21-33)Семестр.cs
@@ -0,0 +1,12 @@
+*** Start programmer edit section *** (Семестр.Интервал CustomAttributes)
+
+*** End programmer edit section *** (Семестр.Интервал CustomAttributes)
+
+*** Start programmer edit section *** (Семестр.Интервал Get)
+                return null;
+*** End programmer edit section *** (Семестр.Интервал Get)
+
+*** Start programmer edit section *** (Семестр.Интервал Set)
+
+*** End programmer edit section *** (Семестр.Интервал Set)
+
