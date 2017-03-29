61dd807233394dd9beed1bfa8915b1e2809d4fcf
Golovin Vladislav <vgolovin@ics.perm.ru>
2017-03-29 15:33:09 +0500

    Added prototype for Task 6

---
+++ Case08/Task 6/BusinessCalendar/ASP.NET/Controls/TimeSpanView/JavaScript/jquery.TSRealTimeLogic.js
@@ -0,0 +1,3 @@
+﻿$(document).ready(function () {
+    $('.TSVContainer').TimeSpans("init", hidden,inputStatus);
+})
---
+++ Case08/Task 6/BusinessCalendar/ASP.NET/Controls/TimeSpanView/TimeSpanView.ascx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Web.UI;
+
+namespace IIS.BusinessCalendar.Controls.TimeSpanView
+{
+    using System.Web.Script.Serialization;
+    using System.Collections.Generic;
+    using ICSSoft.STORMNET;
+    using ICSSoft.STORMNET.Web.Tools;
+
+    public partial class TimeSpanView : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            PageContentManager.AttachExternalFile("/shared/script/jquery-1.7.2.min.js");
+            PageContentManager.AttachExternalFile("/Controls/TimeSpanView/JavaScript/jquery.TimeSpans.js");
+            PageContentManager.AttachExternalFile("/Controls/TimeSpanView/CSS/TimeSpans.css");
+            PageContentManager.AttachExternalFile("/Controls/TimeSpanView/JavaScript/jquery.TSRealTimeLogic.js");
+            if (Page.IsPostBack)
+            {
+                TimeSpansJson.Value = Request[TimeSpansJson.Name];
+            }
+        }
+
+        public List<TimeSpan> Text
+        {
+            get
+            {
+                var jsArray = TimeSpansJson.Value;
+                JavaScriptSerializer ser = new JavaScriptSerializer();
+                return ser.Deserialize<List<TimeSpan>>(jsArray);
+            }
+            set
+            {
+                JavaScriptSerializer ser = new JavaScriptSerializer();
+                TimeSpansJson.Value = ser.Serialize(value);
+            }
+        }
+        public ObjectStatus Status
+        {
+            get
+            {
+                int inputStatus = Convert.ToInt32(TimeSpanViewStatus.Value);
+                ObjectStatus os = new ObjectStatus();
+                switch (inputStatus)
+                {
+                    case 0:
+                        os = ObjectStatus.UnAltered;
+                        break;
+                    case 1:
+                        os = ObjectStatus.Altered;
+                        break;
+                    default:
+                        os = ObjectStatus.Created;
+                        break;
+                }
+                return os;
+            }
+        }
+    }
+}
