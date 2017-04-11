8e793b9a0d19a8685f85d95ca377d31a0d64f66e
unknown <vgolovin@ICS-KK14562.ics.perm.ru>
2017-04-11 20:00:29 +0500

    Добавил решение для Задания 8

---
+++ Case08/Task 8/BusinessCalendar/ASP.NET/Controls/Calendar/CalendarView.ascx
@@ -0,0 +1,14 @@
+﻿<%@ Control Language="C#" 
+    AutoEventWireup="true" 
+    CodeBehind="CalendarView.ascx.cs" 
+    Inherits="IIS.BusinessCalendar.Controls.Calendar.CalendarView" %>
+<div id="CalendarControlContainer">
+    <input type="hidden" id="CalendarDataSource" runat="server" value="" />
+    <div id="CalendarView">
+
+    </div>
+</div>
+
+<script>
+    $('#CalendarView').CalendarView("init",document.getElementById('<%= CalendarDataSource.ClientID%>'));
+</script>
---
+++ Case08/Task 8/BusinessCalendar/ASP.NET/WOLVSettApplyer.cs
@@ -0,0 +1,28 @@
+﻿namespace ICSSoft.STORMNET.Web
+{
+    /// <summary>
+    /// Тюнер для WOLV'а доступный для прикладных программистов.
+    /// </summary>
+    public class WOLVSettApplyer : AjaxControls.WolvSettApplyer
+    {
+        /// <summary>
+        /// Применение настроек внешнего вида для WOLV'а с прикладной логикой.
+        /// Важно: перед применением настроек должен вызываться базовый (технологический)
+        /// метод SettingsApply.
+        /// </summary>
+        /// <param name="wolv">WOLV который необходимо настроить.</param>
+        public override void SettingsApply(AjaxControls.WebObjectListView wolv)
+        {
+            base.SettingsApply(wolv);
+        }
+
+        /// <summary>
+        /// Метод применения настроек, вызывающийся в случае, когда WOLV находится на
+        /// форме лукапа.
+        /// </summary>
+        /// <param name="wolv">WOLV, к которому надо применить настройки.</param>
+        public override void ApplyLookUpSettings(AjaxControls.WebObjectListView wolv)
+        {
+        }
+    }
+}
