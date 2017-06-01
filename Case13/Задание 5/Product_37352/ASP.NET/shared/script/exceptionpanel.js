84adabf91ff3f8e8c2228661821f991798900a3a
PashaMasalkin <pashamasalkin@yandex.ru>
2017-06-01 16:36:52 +0500

    Add task5

---
+++ Case13/Задание 5/Product_37352/ASP.NET/shared/script/exceptionpanel.js
@@ -0,0 +1,14 @@
+﻿    function OnErrorChanged(ctrl, arr)
+    {
+        for(var i = 0; i < arr.length; i++)
+        {
+            if(i == ctrl.value)
+            {
+                document.getElementById(arr[i]).style.display = "block";
+            }
+            else
+            {
+                document.getElementById(arr[i]).style.display = "none";
+            }
+        }
+    }
