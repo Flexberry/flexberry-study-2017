480a4f11d3d2d8e19fb77da316b31722a185cf86
PashaMasalkin <pashamasalkin@yandex.ru>
2017-06-02 17:15:50 +0500

    Add task 6

---
+++ Case13/Задание 6/Агрегатор контактов/ASP.NET/App_Start/ServiceConfig.cs
@@ -0,0 +1,45 @@
+﻿namespace ICSSoft.STORMNET.Web
+{
+    using ICSSoft.Services;
+    using ICSSoft.STORMNET.Business.Audit;
+    using ICSSoft.STORMNET.Web.AjaxControls;
+    using ICSSoft.STORMNET.Web.Tools;
+
+    using Microsoft.Practices.Unity;
+
+    using NewPlatform.Flexberry.Services;
+    using NewPlatform.Flexberry.Web.Themeable;
+
+    /// <summary>
+    /// Класс конфигурации сервисов приложения.
+    /// </summary>
+    public static class ServiceConfig
+    {
+        /// <summary>
+        /// Метод для конфигурации всех сервисов прилоежния.
+        /// </summary>
+        public static void ConfigureServices()
+        {
+            IUnityContainer container = UnityFactory.CreateContainer();
+            
+            // Сервис аудита.
+            AuditSetter.InitAuditService(BridgeToDS.GetDataService());
+
+            // Сервис тем оформления.
+            ThemeService.Current = container.Resolve<IThemeService>();
+            
+            // Настройки лукапа.
+            BaseMasterEditorLookUp.ChangeLookUpSettings = FormUtils.ChangeLookUpSettings;
+
+            // Менеджер расширенных ограничений.
+            if (container.IsRegistered<IAdvLimitManager>())
+                AdvLimitManager.Current = container.Resolve<IAdvLimitManager>();
+
+            // Сервис настроек пользователя.
+            if (container.IsRegistered<IUserSettingsService>())
+                UserSettingsService.Current = container.Resolve<IUserSettingsService>();
+            else
+                LogService.LogWarn("IUserSettingsService не сконфигурирован в Unity. Будет использована реализация по умолчанию.");
+        }
+    }
+}
---
+++ Case13/Задание 6/Агрегатор контактов/ASP.NET/ErrorForm.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="ErrorForm.aspx.cs"
+    Inherits="ICSSoft.STORMNET.Web.ErrorForm" Title="Произошла ошибка" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="ics-form ics-form-list">
+        <h2>
+            <asp:Literal ID="Literal1" runat="server" Text="<%$ Resources: Resource, Error_Occurred %>"></asp:Literal></h2>
+        <div class="ics-form-row">
+            <asp:Label ID="MessageLabel" runat="server"></asp:Label>
+        </div>
+        <div class="ics-form-row">
+            <asp:Label ID="ErrorNumCaption" CssClass="ics-form-label" runat="server" Text="<%$ Resources: Resource, Displayed_Error %>"></asp:Label>
+            <div class="ics-form-controls">
+                <select id="SelectErr" runat="server" class="span2"></select>
+            </div>
+        </div>
+        <asp:Panel ID="Panel4Controls" runat="server">
+        </asp:Panel>
+    </div>
+</asp:Content>
+<asp:Content ContentPlaceHolderID="ContentPlaceHolder0" ID="Content0" runat="server">
+    <asp:Literal ID="ScriptHolder" runat="server" />
+    <script type="text/javascript">
+        function OnErrorChanged(ctrl, arr) {
+            for (var i = 0; i < arr.length; i++) {
+                if (i == ctrl.value) {
+                    document.getElementById(arr[i]).style.display = "block";
+                }
+                else {
+                    document.getElementById(arr[i]).style.display = "none";
+                }
+            }
+        }
+    </script>
+</asp:Content>
